6c747f2ee03a789dec87e14de41fa8fc3bd9d9d1
clarknoah <noahbc08@gmail.com>
2019-09-16 15:27:19 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
 
-console.log("Card Flipped: "+cardFour);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardThree = cards[2];
+var cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }else {
+    alert("Sorry, try next time!");
+  }
+}
